df25cecd2c51c72d329d8e857532fb54fd55839f
forLoop94 <charlesuwem94@gmail.com>
2023-04-10 13:26:46 +0100

    Creat modal delete button in the DOM and append it to the page

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 const mobileVersionContainer = document.querySelector('[data-main]');
 const menuButton = document.querySelector('[data-menu]');
+
+const deleteButton = document.createElement('div');
+deleteButton.textContent = 'X';
+deleteButton.id = 'modal-delete';
+modal.appendChild(deleteButton);
