1bd2833b8b03d74517e329cc59e627f97aa1237b
techmoves <muhndlovu28@gmail.com>
2023-04-10 14:34:28 +0200

    Add  click link to to work section

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ modal.appendChild(deleteButton);
 
 const menuList = document.createElement('ul');
 menuList.id = 'menu-list';
+
+const item1 = document.createElement('li');
+  const portfolio = document.createElement('a');
+  portfolio.href = '#works-section';
+  portfolio.className = 'menu-content';
+  portfolio.textContent = 'Portfolio';
+  item1.appendChild(portfolio);
+  portfolio.addEventListener('click', () => {
+    modal.style.visibility = 'hidden';
+  });
