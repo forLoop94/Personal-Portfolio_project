dcc63491bc1e64423e0ac5422edb8b13bd6dc881
forLoop94 <charlesuwem94@gmail.com>
2023-04-12 12:02:12 +0100

    Add recurringCards function to add cards dynamically

--- index.js
+++ index.js
@@ -251,3 +251,29 @@ const projectData = [
   }
 ]
 
+function recurringCards(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    const card = document.createElement('div');
+    card.className = 'grid-item';
+    const cardDetails = arr[i];
+    card.innerHTML = `
+    <img src=${cardDetails['Featured image'].mobile.src} alt=${cardDetails['Featured image'].alt} width class="project-cards-mobile">
+    <img class='project-cards-desktop' src=${cardDetails['Featured image'].desktop.src} alt=${cardDetails['Featured image'].alt}>
+    <div class="image-text flex">
+      <div>
+        <h3 class="bold-text flex">${cardDetails.name}</h3>
+      </div>
+      <p class="works-description">${cardDetails.description}</p>
+      <ul class="competencies flex">
+        <li>${cardDetails.technologies.tech1}</li>
+        <li>${cardDetails.technologies.tech2}</li>
+        <li>${cardDetails.technologies.tech3}</li>
+      </ul>
+    </div>
+    <div><button type="button" class="btn-type-b flex">See project</button></div>`
+    cardsContainer.appendChild(card);
+  }
+  return cardsContainer;
+}
+recurringCards(projectData);
+workSection.appendChild(cardsContainer);
