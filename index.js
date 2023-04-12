d981e144877e9398d6f37f0975231c63891185e6
forLoop94 <charlesuwem94@gmail.com>
2023-04-12 11:31:18 +0100

    Add unique card function for desktop version

--- index.js
+++ index.js
@@ -93,3 +93,24 @@ let uniqueCard = () => {
   workSection.appendChild(worksCard);
 }
 uniqueCard();
+
+function worksDesktop() {
+  const worksCardDesktop = document.createElement('div');
+  worksCardDesktop.className = 'works-card-desktop';
+  worksCardDesktop.innerHTML = `
+  <img src="images/Img Placeholder.png" alt="" class="project-cards flex">
+  <div class="extras side-text">
+    <h3 id="bold-text">Multi-Post Stories</h3>
+    <p id="works-description">A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.</p>
+    <ul id="competencies" class="flex">
+      <li>CSS</li>
+      <li>HTML</li>
+      <li>Bootstrap</li>
+      <li>Ruby</li>
+    </ul>
+    <button type="button" class="btn-type-a">See project</button>
+  </div>`
+  workSection.appendChild(worksCardDesktop);
+}
+
+worksDesktop();
