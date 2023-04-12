ae5a62422183bf733bebb369e9d08a84d7b65e94
forLoop94 <charlesuwem94@gmail.com>
2023-04-12 11:28:25 +0100

    Add unique card function to create the unique card dynamically

--- index.js
+++ index.js
@@ -75,3 +75,21 @@ let uniqueCard = () => {
   <div class="crossline"><img src="images/Vector (2).png" alt=""></div>
   `
   workSection.appendChild(workSectionTitle);
+
+  const worksCard = document.createElement('div');
+  worksCard.className = 'works-card';
+  worksCard.innerHTML = `
+    <div class="yoga-placeholder"><img src="images/Img Placeholder.png" alt=""></div>
+    <h3 id="bold-text" class="flex">Multi-Post Stories</h3>
+    <p id="works-description">A daily selection of privately personalized reads; no    accounts or sign-ups required.
+      has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard
+      dummy text.</p>
+    <ul id="competencies" class="flex">
+      <li>CSS</li>
+      <li>HTML</li>
+      <li>JAVASCRIPT</li>
+    </ul>
+    <button type="button" class="btn-type-a flex">See project</button>`
+  workSection.appendChild(worksCard);
+}
+uniqueCard();
