2a6ca184fe1bd60c4b2a6697e5c8c25d714379bd
techmoves <muhndlovu28@gmail.com>
2023-04-12 12:46:06 +0200

    Add project data for project two

--- index.js
+++ index.js
@@ -142,8 +142,30 @@ const projectData = [
     },
     'Link to live version': '#',
     'Link to source': '#'
+  },
+
+  {
+    name: 'Project Two',
+    description: 'You need to update the main page so that the projects section is created dynamically using the information stored in that JavaScript object. Remember, all of the HTML in that section is created when the page loads.',
+    'Featured image': {
+      mobile: {
+        src: 'images/Img-Placeholder(1).png',
+        alt: '',
+      },
+      desktop: {
+        src: 'images/Img-Placeholder(3).png',
+        alt: '',
+      }
+    },
+    technologies: {
+      tech1: 'CSS',
+      tech2: 'HTML',
+      tech3: 'Javascript'
+    },
+    'Link to live version': '#',
+    'Link to source': '#'
   }
 
-  
+
 ]
 
